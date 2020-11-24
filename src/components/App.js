750d09357bd31a73dc0b7ef791fa30a58f50cf9b
Tanmay Rathod <tanmay.3mystic@gmail.com>
2020-11-24 10:59:27 +0530

    gfdg

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,67 @@ class App extends Component {
         super(props)
         this.state = {
             renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
+            positionL : 0,
+            positionT : 0,
+            ballPosition: { left: "0px" , top: "0px" } 
         };
+        this.keyListener = this.keyListener.bind(this)
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
     };
 
+    keyListener(e){
+        e.preventDefault();
+        // console.log(e.key);
+        console.log(this.state.position);
+        if(e.key == "ArrowRight") {
+            this.setState(
+                {
+                    positionL : this.state.positionL + 5,
+                    ballPosition:{ left: `${this.state.positionL}px` ,  top: `${this.state.positionT}px`}
+                }
+            )
+        }
+        if(e.key == "ArrowLeft") {
+            this.setState(
+                {
+                    positionL : this.state.positionL -5 ,
+                    ballPosition:{ left: `${this.state.positionL}px` ,  top: `${this.state.positionT}px`}
+                }
+            )
+        }
+        if(e.key == "ArrowDown") {
+            this.setState(
+                {
+                    positionT : this.state.positionT+5 ,
+                    ballPosition:{ left: `${this.state.positionL}px` , top: `${this.state.positionT}px`}
+                }
+            )
+        }
+        if(e.key == "ArrowUp") {
+            this.setState(
+                {
+                    positionT : this.state.positionT - 5 ,
+                    ballPosition:{left: `${this.state.positionL}px` ,  top: `${this.state.positionT}px`}
+                }
+            )
+        }
+
+       
+    }
+
     buttonClickHandler() {
-   
-   }
+         this.setState({renderBall:true});
+         document.addEventListener("keydown" , this.keyListener );
+    }
     renderBallOrButton() {
+        
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
 		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-		}
+        }
+
     }
 
     // bind ArrowRight keydown event
